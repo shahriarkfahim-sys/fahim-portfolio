a77efd2570bbf030f4246d06dee018df0c0ebba3
shahriarkfahim-sys <shahriar.k.fahim@gmail.com>
2026-08-22 18:44:42 +0600

    update

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.get('/api/medium-feed', async (_req, res) => {
 });
 
 app.get('/dashboard', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'task-dashboard', 'Task Dashboard.html'));
+  res.sendFile(path.join(__dirname, 'dashboard', 'index.html'));
 });
 
 app.get('/dashboard/:path*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'task-dashboard', 'Task Dashboard.html'));
+  res.sendFile(path.join(__dirname, 'dashboard', 'index.html'));
 });
 
 app.use(express.static(path.join(__dirname)));
